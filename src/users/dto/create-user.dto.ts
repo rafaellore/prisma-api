26b0186673951368d14945e8064d9cfb25d911c0
rafaellore <birdneon3@gmail.com>
2025-03-26 19:21:51 -0300

    refact(create-user.dto): adds validation data

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
-  name: string;
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
   admin: boolean;
 }
