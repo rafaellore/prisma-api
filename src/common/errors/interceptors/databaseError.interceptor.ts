a43569355c413892da9ae97f988e9f9248186d01
rafaellore <birdneon3@gmail.com>
2025-04-09 17:31:53 -0300

    feat(interceptors): adds initial structure for database errors

---
+++ src/common/errors/interceptors/databaseError.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { isPrismaError } from 'src/common/utils/is-prisma-client.utils';
+
+@Injectable()
+export class DatabaseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(error => {
+        if (isPrismaError(error)) {
+          /* empty */
+        }
+        throw error;
+      }),
+    );
+  }
+}
